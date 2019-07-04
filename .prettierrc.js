8bd27653de119f5eee55f544dfdccf48e1cb128d
谷慧 <879910950@qq.com>
2019-07-04 21:31:13 +0800

    初始化项目

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+/**
+ * Prettier 配置文件
+ * 官网：https://prettier.io/
+ * 配置文档：https://prettier.io/docs/en/options.html
+ */
+
+module.exports = {
+  /**
+   * 单行最大长度
+   * https://prettier.io/docs/en/options.html#print-width
+   */
+  printWidth: 100,
+
+  /**
+   * 使用单引号
+   * https://prettier.io/docs/en/options.html#quotes
+   */
+  singleQuote: true,
+
+  /**
+   * 尾随逗号
+   * https://prettier.io/docs/en/options.html#trailing-commas
+   */
+  trailingComma: 'all',
+};
